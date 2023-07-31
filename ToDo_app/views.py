b1aa3ea94c02ad7cc5965671764d69d76235da70
Jaypatel123 <jaypatel97043@gmail.com>
2023-07-31 15:08:29 -0400

    Progress update

--- ToDo_app/views.py
+++ ToDo_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+# from Templates.ToDo_app import list
 # Create your views here.
+
+def index(request):
+    # return HttpResponse("Hello World")
+    return render(request, 'ToDo_app/list.html')
